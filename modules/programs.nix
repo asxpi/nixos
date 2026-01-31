ec89f1d0ce54685bf1158c420ec10aa579fb9d01
Sergei Poljanski <me@asxp.io>
2026-01-31 06:04:09 +0200

    nix ld

--- modules/programs.nix
+++ modules/programs.nix
@@ -33,6 +33,19 @@
     dedicatedServer.openFirewall = true;
   };
 
+  programs.nix-ld.enable = true;
+
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+  ];
+
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
